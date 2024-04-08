6ba8a5c3752cb0d3d5c071a6a5065b0a05ab37cd
Lucas C Barros <Lucas_cb@icloud.com>
2024-04-08 16:47:13 -0700

    adding init in package

--- Sources/DebugTools/DebugTools.swift
+++ Sources/DebugTools/DebugTools.swift
@@ -1,7 +1,13 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+
 public struct DebugTools {
+    
+    public init() {
+        
+    }
+    
     public func helloToYou() -> String {
         return "Hello to You!"
     }
